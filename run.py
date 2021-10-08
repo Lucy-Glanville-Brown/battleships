6c47fa9ff51962eae86bf5864de0954c28ad69f6
Lucy-Glanville-Brown <lucyglanvillebrown@gmail.com>
2021-10-08 09:38:19 +0000

    Add print_board function to create a board with letters for columns and numbers for rows

--- run.py
+++ run.py
@@ -6,9 +6,24 @@ from random import randint
 # "-" for missed shot
 
 HIDDEN_BOARD = [[" "] * 8 for x in range(8)]
+# creates a list of 8 spaces, 8 times
 GUESS_BOARD = [[" "] * 8 for x in range(8)]
+# creates a list of 8 spaces, 8 times
 
-print("hidden board")
-print(HIDDEN_BOARD)
-print("guess board")
-print(GUESS_BOARD)
+
+def print_board(board):
+    """
+    Creates a board with letters for the columns and numbers for the rows with | separator
+
+    """
+    print("  A B C D E F G H")
+    print("  ---------------")
+    row_number = 1
+    for row in board:
+        print(row_number, "|".join(row))
+        row_number += 1
+
+print("print board hidden board")
+print_board(HIDDEN_BOARD)
+print("print board guess board")
+print_board(GUESS_BOARD)
