b51fc67e58f5d680fe88a40f481594767a385cd6
Lucy-Glanville-Brown <lucyglanvillebrown@gmail.com>
2021-10-08 09:22:54 +0000

    Add HIDDEN_BOARD and GUESS_BOARD variables which creates a list of 8 spaces, 8 times

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+# Legend
+# "X" for placing ship and hit battleship
+# " " for available space
+# "-" for missed shot
+
+HIDDEN_BOARD = [[" "] * 8 for x in range(8)]
+GUESS_BOARD = [[" "] * 8 for x in range(8)]
+
+print("hidden board")
+print(HIDDEN_BOARD)
+print("guess board")
+print(GUESS_BOARD)
